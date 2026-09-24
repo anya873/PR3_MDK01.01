2c8d428efb9f0692802e24f6d63f4437e220e8b8
Shapovalova Anna <sapovalovaanna873@gmail.com>
2026-09-24 11:34:35 +0500

    Разработанный класс. Шаповалова.

--- WpfApp1/Glasses/PersonInfo.cs
+++ WpfApp1/Glasses/PersonInfo.cs
@@ -8,6 +8,23 @@ namespace WpfApp1.Glasses
 {
     internal class PersonInfo
     {
-        
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int Level { get; set; }
+        public int Glasses { get; set; }
+        public int Money { get; set; }
+        public float Damage { get; set; }
+
+        public PersonalInfo(string name, int health, int armor, int level, int glasses, int money, float damage)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Armor = armor;
+            this.Level = level;
+            this.Glasses = glasses;
+            this.Money = money;
+            this.Damage = damage;
+        }
     }
 }
